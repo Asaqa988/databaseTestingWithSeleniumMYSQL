331fbeb66f24fe6560287e5846c9f7b4624b5de1
Asaqa988 <asaqa001@gmail.com>
2024-08-28 18:34:22 +0300

    update

--- src/databaseProject/DataTest.java
+++ src/databaseProject/DataTest.java
@@ -17,7 +17,7 @@ import org.testng.annotations.Test;
 
 public class DataTest {
 
-	WebDriver driver = new ChromeDriver(); 
+	//WebDriver driver = new ChromeDriver();
 	Random rand = new Random();
 	Random rand2 = new Random();
 
@@ -25,20 +25,22 @@ public class DataTest {
 	Statement stmt;
 
 	ResultSet rs;
+	
+	int randomnumber = rand.nextInt(500) ;
 
 	@BeforeTest
 	public void mySetup() throws SQLException {
 		con = DriverManager.getConnection("jdbc:mysql://localhost:3306/classicmodels", "root", "abed");
 	}
 
-	@Test(priority = 1)
+	@Test(priority = 1,invocationCount = 50)
 
 	public void AdddataToThedataBase() throws SQLException {
 
-		int randomnumber = rand.nextInt(889) * rand2.nextInt(453);
+randomnumber = rand.nextInt(500); 
 
 		System.out.println(randomnumber);
-		String query = "insert into customers (customerNumber,customerName,contactLastName,contactFirstName,phone,addressLine1,addressLine2,city,state,postalCode,country,salesRepEmployeeNumber,creditLimit)VALUES ( 501, 'Sunrise Ventures', 'Anderson', 'Michael', '212-555-1234', '123 Main Street', 'Suite 200', 'New York', 'NY', '10001', 'USA', 1621, 50000.00)";
+		String query = "insert into customers (customerNumber,customerName,contactLastName,contactFirstName,phone,addressLine1,addressLine2,city,state,postalCode,country,salesRepEmployeeNumber,creditLimit)VALUES ("+randomnumber+" , 'Sunrise Ventures', 'Anderson', 'Michael', '212-555-1234', '123 Main Street', 'Suite 200', 'New York', 'NY', '10001', 'USA', 1621, 50000.00)";
 
 		stmt = con.createStatement();
 
@@ -50,12 +52,12 @@ public class DataTest {
 
 	@Test(priority = 2)
 	public void updateData() throws SQLException {
-		String query = "update customers set contactFirstName ='nooralhuda' where customerNumber = 501";
+		String query = "update customers set contactFirstName ='nooralhuda' where customerNumber = "+randomnumber;
 
 		stmt = con.createStatement();
 
 		int NumberOfRowUpdated = stmt.executeUpdate(query);
-		
+
 		System.out.println(NumberOfRowUpdated);
 
 	}
@@ -65,7 +67,7 @@ public class DataTest {
 	public void myFirstTestToGetData() throws SQLException {
 
 		stmt = con.createStatement();
-		rs = stmt.executeQuery("select * from customers where customerNumber=501");
+		rs = stmt.executeQuery("select * from customers where customerNumber="+randomnumber);
 
 		while (rs.next()) {
 			int CustomnumberInThedataBase = rs.getInt("customerNumber");
@@ -73,32 +75,29 @@ public class DataTest {
 
 			System.out.println(CustomnumberInThedataBase);
 			System.out.println(CustomerNameInTheDataBase);
-			
+
 			String firstName = rs.getString("contactFirstName");
 			String lastName = rs.getString("contactLastName");
-		
-		
-		
-		driver.get("https://magento.softwaretestingboard.com/customer/account/create/");
-		
-		WebElement firstnameInputField = driver.findElement(By.id("firstname"));
-				WebElement LastNameInputField = driver.findElement(By.id("lastname"));
-				
-				firstnameInputField.sendKeys(firstName);
-				LastNameInputField.sendKeys(lastName); 
+
+			//driver.get("https://magento.softwaretestingboard.com/customer/account/create/");
+
+			//WebElement firstnameInputField = driver.findElement(By.id("firstname"));
+		//	WebElement LastNameInputField = driver.findElement(By.id("lastname"));
+
+			//firstnameInputField.sendKeys(firstName);
+			//LastNameInputField.sendKeys(lastName);
 		}
 	}
 
-	@Test(priority = 4,enabled = true)
+	@Test(priority = 4, enabled = false)
 
 	public void DeleteData() throws SQLException {
-		String query = "delete from customers where customerNumber = 501";
+		String query = "delete from customers where customerNumber = "+randomnumber;
 
 		stmt = con.createStatement();
 
 		int numberofRowdeleted = stmt.executeUpdate(query);
-		
-		
+
 		System.out.println(numberofRowdeleted);
 	}
 
